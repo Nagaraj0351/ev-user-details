0f84a63f335d262b7873ebcace0f1a3d1a8f042f
Nagaraj0351 <s.nagaraj035@gmail.com>
2022-08-09 22:07:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const defaultUserInfo = {
+  title: "",
+  firsName: "",
+  lastName: "",
+  email: "",
+};
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [userInfo, setUserInfo] = useState(defaultUserInfo);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    console.log("rendering");
+    setLoading(true);
+    axios.get("https://randomuser.me/api").then((response) => {
+      const {
+        data: {
+          results: [
+            {
+              name: { first, last, title },
+              email,
+            },
+          ],
+        },
+      } = response;
+
+      const resUserInfo = {
+        title,
+        firsName: first,
+        lastName: last,
+        email,
+      };
+
+      setUserInfo(resUserInfo);
+
+      if (window) localStorage.setItem("userInfo", JSON.stringify(resUserInfo));
+
+      setLoading(false);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Details</h1>
+      {!loading ? (
+        <>
+          <div className="user-details--container">
+            <div className="name">
+              <strong>Name:</strong>{" "}
+              <span>
+                {userInfo.title}. {userInfo.firsName} {userInfo.lastName}
+              </span>
+            </div>
+            <div className="email">
+              <strong>Email:</strong> <span>{userInfo.email}</span>
+            </div>
+          </div>
+          <button className="refresh-btn" onClick={fetchData}>
+            Refresh
+          </button>
+        </>
+      ) : (
+        <div>Fetching data</div>
+      )}
     </div>
   );
 }
